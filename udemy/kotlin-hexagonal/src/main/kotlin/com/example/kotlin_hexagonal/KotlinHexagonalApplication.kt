4018e40c62b200f2084ef0fe023d5110cfcbcbf3
Raphael Giron <maingiron@gmail.com>
2025-06-08 12:40:08 -0300

    init

---
+++ udemy/kotlin-hexagonal/src/main/kotlin/com/example/kotlin_hexagonal/KotlinHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_hexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinHexagonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinHexagonalApplication>(*args)
+}
