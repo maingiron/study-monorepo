f76954c17baa2338bb039214e609f5bbede20073
Raphael Giron <maingiron@gmail.com>
2025-11-03 22:38:23 -0300

    add Swagger

--- my-projects/spring-labs/src/main/kotlin/com/example/spring_labs/controller/CepController.kt
+++ my-projects/spring-labs/src/main/kotlin/com/example/spring_labs/controller/CepController.kt
@@ -1,19 +1,27 @@
 package com.example.spring_labs.controller
 
 import com.example.spring_labs.service.CepService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Consulta de CEP", description = "Serviço para buscar informações de CEPs brasileiros.")
 @RestController
 @RequestMapping("/v1/cep")
 class CepController(
     private final val cepService: CepService
 ) {
 
+    @Operation(summary = "Buscar endereço por CEP.", description = "Consulta o ViaCEP e retorna informações do endereço.")
     @GetMapping("/{cep}")
-    fun getCep(@PathVariable cep: String): String {
+    fun getCep(
+        @Parameter(description = "CEP com 8 dígitos numéricos.", example = "01001000")
+        @PathVariable cep: String
+        ): String {
         return cepService.getCep(cep)
     }
 }
